52a1e300e866ba11dc85651c4446ddd2c76c38e2
MizyurkinaOlga <olga.mizyurkina@mail.ru>
2020-03-01 18:10:31 +0700

    BankInterest/Program working version

--- BankInterest/Program.cs
+++ BankInterest/Program.cs
@@ -8,8 +8,23 @@ namespace BankInterest
 {
     class Program
     {
+        public static double Calculate(string userInput)
+        {
+            string[] elements = userInput.Split(' ');
+            double s = Convert.ToDouble(elements[0]);
+            double r = Convert.ToDouble(elements[1]);
+            int n = Convert.ToInt32(elements[2]);            
+            double p = 1 + (r / 12.0) / 100.0;
+            return s*Math.Pow(p,n);
+        }
         static void Main(string[] args)
         {
+            string userInput;
+            userInput = Console.ReadLine();
+            double deposit;
+            deposit = Calculate(userInput);
+            Console.WriteLine(deposit);
+            Console.ReadKey();
         }
     }
 }
